261e4feb74da1aa8a157bb0a9b9048055520dbee
Liubou Levitskaya <lyubasaraeva@gmail.com>
2024-10-14 01:30:00 +0300

    feat: implement winner check

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -217,6 +217,50 @@ function makeWinningMove() {
     return false; 
 }
 
+//!CHECK WINNER
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+function checkWinner() {
+    for (const condition of winningConditions) {
+        const [a, b, c] = condition;
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            return gameBoard[a];
+        }
+    }
+    return null;
+}
+
+function findWinningCombination() {
+    for (const condition of winningConditions) {
+        const [a, b, c] = condition;
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            return condition;
+        }
+    }
+    return null;
+}
+
+function highlightWinningCombination(winningCombination) {
+    winningCombination.forEach(index => {
+        boxes[index].classList.add('winning-cell');
+    });
+}
+
+function highlightDraw() {
+    boxes.forEach(box => {
+        box.classList.add('draw-field');
+    });
+}
+
 
 
 
